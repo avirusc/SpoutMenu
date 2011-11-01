f4618e367bec1b90bd712cb38eb66b12922f65b4
avirusc <jerry.langley@lonestar.edu>
2011-11-01 14:46:21 -0500

    Start over.

--- src/me/avirusc/SpoutMenu/SpoutMenu.java
+++ src/me/avirusc/SpoutMenu/SpoutMenu.java
@@ -12,31 +12,6 @@ public class SpoutMenu extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");	
 	
 	public void onEnable(){
-		
-		config = getConfig();
-		
-		this.getConfig().set("command1", "/commandname");
-		this.getConfig().set("command2", "/commandname");
-		this.getConfig().set("command3", "/commandname");
-		this.getConfig().set("command4", "/commandname");
-		this.getConfig().set("command5", "/commandname");
-		this.getConfig().set("command6", "/commandname");
-		this.getConfig().set("command7", "/commandname");
-		this.getConfig().set("command8", "/commandname");
-		this.getConfig().set("command9", "/commandname");
-		this.getConfig().set("command10", "/commandname");
-
-		String sProp1 = this.getConfig().getString("command1","no");
-		String sProp2 = this.getConfig().getString("command2","no");
-		String sProp3 = this.getConfig().getString("command3","no");
-		String sProp4 = this.getConfig().getString("command4","no");
-		String sProp5 = this.getConfig().getString("command5","no");
-		String sProp6 = this.getConfig().getString("command6","no");
-		String sProp7 = this.getConfig().getString("command7","no");
-		String sProp8 = this.getConfig().getString("command8","no");
-		String sProp9 = this.getConfig().getString("command9","no");
-		String sProp10 = this.getConfig().getString("command10","no");
-		
 		getServer().getPluginManager().registerEvent(Type.CUSTOM_EVENT, new SMInputListener(), Priority.Normal, this);
 		getServer().getPluginManager().registerEvent(Type.CUSTOM_EVENT, new SMScreenListener(), Priority.Normal, this);
 		
