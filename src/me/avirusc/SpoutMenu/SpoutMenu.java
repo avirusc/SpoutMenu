023c30fe2ff071644de0f87bb1643d9636b8f02b
avirusc <jerry.langley@lonestar.edu>
2011-11-01 13:31:03 -0500

    commit2

--- src/me/avirusc/SpoutMenu/SpoutMenu.java
+++ src/me/avirusc/SpoutMenu/SpoutMenu.java
@@ -6,10 +6,34 @@ import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpoutMenu extends JavaPlugin {
+	protected FileConfiguration config;
 
 	Logger log = Logger.getLogger("Minecraft");	
 	
 	public void onEnable(){
+		this.getConfig().set("command1", "/commandname");
+		this.getConfig().set("command2", "/commandname");
+		this.getConfig().set("command3", "/commandname");
+		this.getConfig().set("command4", "/commandname");
+		this.getConfig().set("command5", "/commandname");
+		this.getConfig().set("command6", "/commandname");
+		this.getConfig().set("command7", "/commandname");
+		this.getConfig().set("command8", "/commandname");
+		this.getConfig().set("command9", "/commandname");
+		this.getConfig().set("command10", "/commandname");
+
+		string sProp = this.getconfig().getString("command1","no");
+		string sProp = this.getconfig().getString("command2","no");
+		string sProp = this.getconfig().getString("command3","no");
+		string sProp = this.getconfig().getString("command4","no");
+		string sProp = this.getconfig().getString("command5","no");
+		string sProp = this.getconfig().getString("command6","no");
+		string sProp = this.getconfig().getString("command7","no");
+		string sProp = this.getconfig().getString("command8","no");
+		string sProp = this.getconfig().getString("command9","no");
+		string sProp = this.getconfig().getString("command10","no");
+		
+		config = getConfig();
 		
 		getServer().getPluginManager().registerEvent(Type.CUSTOM_EVENT, new SMInputListener(), Priority.Normal, this);
 		getServer().getPluginManager().registerEvent(Type.CUSTOM_EVENT, new SMScreenListener(), Priority.Normal, this);
