b843813324565fd5179e61a02eef43233e12994f
avirusc <jerry.langley@lonestar.edu>
2011-11-08 15:57:37 -0600

    credit skawke with idea in code

--- src/me/avirusc/SpoutMenu/SpoutMenu.java
+++ src/me/avirusc/SpoutMenu/SpoutMenu.java
@@ -14,6 +14,7 @@
 //You should have received a copy of the GNU General Public License
 //along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+// Special thanks to skawke and SpoutEssentials for inspiring me to make this.
 // Special thanks to NeatMonster for his custom key code used in SpoutBackpack.
 
 package me.avirusc.SpoutMenu;
